af7032395919e964c43af3eb72f7d4f81fbf1894
DyAnde <21dyande@gmail.com>
2023-04-25 15:11:25 -0500

    ugh

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import (
-    abort, Flask, jsonify, redirect, render_template, request,
-    session, url_for
+        abort, Flask, jsonify, redirect, render_template, request,
+        session, url_for
 )
 import os 
 
@@ -12,14 +12,20 @@ def index():
 
 @app.route('/fortune/')
 def fortune():
-	output = os.system('fortune')
-    return '<pre>' + output + '</pre>' 
+    #output = os.system('fortune')
+    #return '<pre>' + str(output) + '</pre>' 
+    return 'THIS IS WHERE FORTUNE WOULD BE IF IT WORKED'
 
 @app.route('/cowsay/<message>/')
 def cowsay(message):
-    return os.system(f'cowsay({message})')
+    os.system(f'cowsay({message}) > temp.txt')
+    with open('temp.txt', 'r') as f:
+        ahhh = f.read()
+    os.remove('temp.txt')
+    return '<pre>' + ahhh + '</pre>'
 
 @app.route('/cowfortune/')
 def cowfortune():
-	output = os.system('fortune')
-	return os.system(f'cowsay({output})')
+    #output = os.system('fortune')
+    #return os.system(f'cowsay({output})')
+    return 
